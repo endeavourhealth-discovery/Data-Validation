2399c83ee3fe4764a4685ab5c85981a33c403f85
Drew Littler <drewlittler@hotmail.com>
2018-09-11 12:00:18 +0100

    Adding support for new date formats when searching by DoB and now handles named with hyphen properly

--- API/src/main/java/org/endeavourhealth/dataassurance/helpers/SearchTermsParser.java
+++ API/src/main/java/org/endeavourhealth/dataassurance/helpers/SearchTermsParser.java
@@ -1,6 +1,9 @@
 package org.endeavourhealth.dataassurance.helpers;
 
+import com.google.common.base.Strings;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -10,6 +13,10 @@ import java.util.Date;
 import java.util.List;
 
 public class SearchTermsParser {
+    private static final Logger LOG = LoggerFactory.getLogger(SearchTermsParser.class);
+
+    private static List<String> cachedDateFormats = null;
+
     private String nhsNumber;
     private String emisNumber;
     private Date dateOfBirth;
@@ -31,27 +38,69 @@ public class SearchTermsParser {
 
         for (String token : tokens) {
 
-            if (isDate(token)) {
-                // Assume its a date dd-MMM-yyyy & attempt to parse
-                SimpleDateFormat sf = new SimpleDateFormat("dd-MMM-yyyy");
-                try {
-                    dateOfBirth = sf.parse(token);
-                } catch (ParseException e) {
-                    // Not a valid date, continue and treat as regular token
-                }
-            } else if (!StringUtils.isAlpha(token)) {
-                if (token.length() == 10)
-                    this.nhsNumber = token;
-                else
-                    this.emisNumber = token;
-            } else {
-                this.names.add(token);
+            //try treating as a date
+            dateOfBirth = tryParseDate(token);
+            if (dateOfBirth != null) {
+                continue;
             }
+
+            //if contains any digits, try treating as an ID
+            if (containsDigit(token)) {
+
+                //if ten chars and purely digits, then treat as NHS number
+                if (token.length() == 10
+                        && StringUtils.isNumeric(token)) {
+                    this.nhsNumber = token;
+
+                } else {
+                    this.emisNumber = token;
+                }
+                continue;
+            }
+
+            //otherwise treat as a name token
+            this.names.add(token);
         }
     }
 
-    public boolean isDate(String token) {
-        return (token.length() == 10 || token.length() == 11) && StringUtils.countMatches(token, "-")==2;
+    private static boolean containsDigit(String token) {
+        for (char c: token.toCharArray()) {
+            if (Character.isDigit(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static List<String> getDateFormats() {
+        if (cachedDateFormats == null) {
+            List<String> l = new ArrayList<>();
+            //l.add("ddMMyyyy"); removed as this will be confused with local IDs
+            l.add("dd-MMM-yyyy");
+            l.add("dd-MM-yyyy");
+            l.add("dd/MMM/yyyy");
+            l.add("dd/MM/yyyy");
+
+            cachedDateFormats = l;
+        }
+        return cachedDateFormats;
+    }
+
+    private static Date tryParseDate(String token) {
+        for (String format: getDateFormats()) {
+
+            //compare length, as SimpleDateFormat doesn't validate it
+            if (format.length() == token.length()) {
+                SimpleDateFormat sf = new SimpleDateFormat(format);
+                try {
+                    return sf.parse(token);
+                } catch (ParseException e2) {
+                    //ignore the error
+                }
+            }
+        }
+
+        return null;
     }
 
     public boolean hasNhsNumber() {
@@ -120,5 +169,64 @@ public class SearchTermsParser {
 
     public boolean hasNames() {
         return names.size() > 0;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Parsed Search Terms: ");
+
+        if (!Strings.isNullOrEmpty(nhsNumber)) {
+            sb.append("NHS Number: [");
+            sb.append(nhsNumber);
+            sb.append("] ");
+        }
+
+        if (!Strings.isNullOrEmpty(emisNumber)) {
+            sb.append("Patient Number: [");
+            sb.append(emisNumber);
+            sb.append("] ");
+        }
+
+        if (dateOfBirth != null) {
+            sb.append("Date of Birth: ");
+            sb.append(dateOfBirth);
+        }
+
+        if (!names.isEmpty()) {
+            sb.append("Names: ");
+            for (String name: names) {
+                sb.append(name);
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static String testParsing() {
+        try {
+
+            List<String> tests = new ArrayList<>();
+            tests.add("24/05/1990"); //DoB
+            tests.add("24-05-1990"); //DoB
+            tests.add("24/MAY/1990"); //DoB
+            tests.add("24-MAY-1990"); //DoB
+            tests.add("1234567890"); //NHS number
+            tests.add("24051978"); //patient ID
+            tests.add("12123-132123-12312"); //patient ID
+            tests.add("firstname lastname"); //names
+            tests.add("firstname middlename lastname"); //names
+            tests.add("firstname de-lastname"); //names with hyphen
+
+            for (String test: tests) {
+                SearchTermsParser parser = new SearchTermsParser(test);
+                LOG.debug("from [" + test + "] -> " + parser);
+            }
+
+        } catch (Throwable t) {
+            LOG.error("", t);
+        }
     }
 }
