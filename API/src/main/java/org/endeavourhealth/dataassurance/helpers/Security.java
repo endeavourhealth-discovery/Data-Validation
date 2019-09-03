4156bf999e1de3a0c1a60108027b200bf18da942
Drew Littler <drewlittler@hotmail.com>
2019-09-03 09:26:27 +0100

    Adding support for ODS code in the Keycloak security role checking, in place of service UUIDs

--- API/src/main/java/org/endeavourhealth/dataassurance/helpers/Security.java
+++ API/src/main/java/org/endeavourhealth/dataassurance/helpers/Security.java
@@ -1,13 +1,21 @@
 package org.endeavourhealth.dataassurance.helpers;
 
+import com.google.common.base.Strings;
 import org.endeavourhealth.common.security.SecurityUtils;
+import org.endeavourhealth.core.database.dal.DalProvider;
+import org.endeavourhealth.core.database.dal.admin.ServiceDalI;
+import org.endeavourhealth.core.database.dal.admin.models.Service;
 import org.endeavourhealth.coreui.endpoints.UserManagerEndpoint;
 import org.keycloak.representations.AccessToken;
 
 import javax.ws.rs.core.SecurityContext;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Security {
+
+    private static Map<String, UUID> hmOdsCodeToUuidMap = new ConcurrentHashMap<>();
+
     public Set<String> getUserAllowedOrganisationIdsFromSecurityContext(SecurityContext securityContext, String projectId) {
         if (projectId != null) {
             try {
@@ -25,20 +33,56 @@ public class Security {
         if (orgGroups != null) {
             for (Object orgGroup1 : orgGroups) {
                 Map<String, Object> orgGroup = (Map) orgGroup1;
-                String orgGroupOrganisationId = (String) orgGroup.getOrDefault("organisationId", null);
-                if (orgGroupOrganisationId != null)
-                    orgs.add(orgGroupOrganisationId);
+
+                String orgGroupOrganisationId = (String)orgGroup.getOrDefault("organisationId", null);
+                UUID uuid = findServiceIdForKeyCloakValue(orgGroupOrganisationId);
+                if (uuid != null) {
+                    orgs.add(uuid.toString());
+                }
             }
         }
 
         return orgs;
     }
 
+    private UUID findServiceIdForKeyCloakValue(String keyCloakOrgId) {
+
+        if (Strings.isNullOrEmpty(keyCloakOrgId)) {
+            return null;
+        }
+
+        UUID serviceUuid = null;
+
+        try {
+            //if it's a UUID, then just use it as is
+            serviceUuid = UUID.fromString(keyCloakOrgId);
+
+        } catch (Exception ex) {
+
+            //if not a UUID then treat as an ODS code and look up via the DB
+            serviceUuid = hmOdsCodeToUuidMap.get(keyCloakOrgId);
+            if (serviceUuid == null) {
+                ServiceDalI serviceDal = DalProvider.factoryServiceDal();
+                try {
+                    Service service = serviceDal.getByLocalIdentifier(keyCloakOrgId);
+                    if (service != null) {
+                        serviceUuid = service.getId();
+                        hmOdsCodeToUuidMap.put(keyCloakOrgId, serviceUuid);
+                    }
+                } catch (Exception ex2) {
+                    throw new RuntimeException("Failed to find service for ODS code " + keyCloakOrgId, ex2);
+                }
+            }
+        }
+
+        return serviceUuid;
+    }
+
     public Set<String> getPublishingOrganisationIdsFromProject(String projectId) throws Exception {
         UserManagerEndpoint um = new UserManagerEndpoint();
         List<String> orgUuids = um.getPublishersForProject(projectId);
 
-        return new HashSet<String>(orgUuids);
+        return new HashSet<>(orgUuids);
 
     }
 }
